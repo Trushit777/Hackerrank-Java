647a509890570ac9590335e78bb159cb5c9afeed
Trushit777 <52493492+Trushit777@users.noreply.github.com>
2020-10-12 08:35:09 +0000

    Add files via upload
    Drawing Book Hacker rank Solution in Java.

---
+++ DrawingBook.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Problem Description -> https://www.hackerrank.com/challenges/drawing-book/problem
+ *
+ * @author @Trushit777
+ * @since 12-10-2020
+ */
+public class DrawingBook {
+
+	/**
+	 * @param n
+	 * @param p
+	 *
+	 * @return
+	 */
+	static int pageCount(int n, int p) {
+		if(p > n/2.0){
+			return p%2==0 ? (n-p)/2 : (n-p+1)/2;
+		} else {
+			return p%2 == 0 ? ((p-1) / 2)+1 : (p-1)/2;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int total = scan.nextInt();
+		int reach = scan.nextInt();
+		System.out.println(pageCount(total,reach));
+	}
+}
