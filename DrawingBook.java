4500921c8c7227833ea18bf4a5d6b48c48a5df15
Trushit777 <52493492+Trushit777@users.noreply.github.com>
2020-10-12 08:38:25 +0000

    Update DrawingBook.java

--- DrawingBook.java
+++ DrawingBook.java
@@ -16,9 +16,9 @@ public class DrawingBook {
 	 */
 	static int pageCount(int n, int p) {
 		if(p > n/2.0){
-			return p%2==0 ? (n-p)/2 : (n-p+1)/2;
+			return p%2 == 0 ? (n-p) / 2 : (n-p+1) / 2;
 		} else {
-			return p%2 == 0 ? ((p-1) / 2)+1 : (p-1)/2;
+			return p%2 == 0 ? ((p-1) / 2) + 1 : (p-1) / 2;
 		}
 	}
 	public static void main(String[] args) {
